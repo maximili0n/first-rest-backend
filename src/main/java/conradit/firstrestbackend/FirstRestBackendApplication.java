5ce09a4c2c069362c20f60d3485e4508eea89395
Maximilian Conrad <maxi@Maximilians-MBP.fritz.box>
2020-10-30 12:09:31 +0100

    init commit

---
+++ src/main/java/conradit/firstrestbackend/FirstRestBackendApplication.java
@@ -0,0 +1,13 @@
+package conradit.firstrestbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstRestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstRestBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/conradit/firstrestbackend/FirstRestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package conradit.firstrestbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstRestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
